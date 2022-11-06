3ae3118dcad3fcff00230e362036b2f3648c7c7b
Harsha Vardhan Koneru <hkoneru@usc.edu>
2022-11-05 22:24:56 -0700

    Update Personal Profile implemented

--- app/src/views/profile/profile.view.js
+++ app/src/views/profile/profile.view.js
@@ -1,15 +1,42 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { userDataByID } from '../../reduxSlices/userDataSlice';
 import ResumeModal from '../../components/ResumeModal/resumeModal.component';
 import InputField from '../../components/InputField/inputField.component';
 import RadioButtonField from '../../components/RadioButton/radioButton.component';
 import { FaPen } from 'react-icons/fa';
 import './profile.scss';
 
+const UPDATE_USER_DATA_MUTATION = gql`
+    mutation updateUserProfile($input: updateProfileInput!) {
+        updateUserProfile(input: $input) {
+            status
+            message
+        }
+    }
+`;
+
+const QUERY_USER_DATA = gql`
+    query getUserByID($userId: ID!) {
+        getUserDataById(userId: $userId) {
+            firstName
+            lastName
+            h1b_required
+            email
+            skills
+            resume_uploaded
+        }
+    }
+`;
+
 export default function ProfileView() {
+    const dispatch = useDispatch();
     const userInfo = useSelector((state) => state?.userData?.value);
+
+    const userId = localStorage.getItem('userId');
 
     const [show, setShow] = useState(false);
     const [editPersonalInfo, setEditPersonalInfo] = useState(false);
@@ -33,6 +60,32 @@ export default function ProfileView() {
 
     const { firstNameValid, lastNameValid, h1bValid } = inputValueValid;
 
+    const nameRegrex = /^[a-zA-Z ]+$/;
+
+    const {
+        data: userData,
+        loading: userLoading,
+        error: userError,
+        refetch
+    } = useQuery(QUERY_USER_DATA, {
+        variables: { userId }
+    });
+
+    const [
+        updateUserProfile,
+        {
+            data: updateUserData,
+            loading: updateUserLoading,
+            error: updateUserError
+        }
+    ] = useMutation(UPDATE_USER_DATA_MUTATION);
+
+    useEffect(() => {
+        if (updateUserData) {
+            dispatch(userDataByID(userData.getUserDataById));
+        }
+    }, [updateUserData]);
+
     const setFunction = (name, value, type) => {
         if (type === 'input') {
             setInputValue((prev) => ({
@@ -48,7 +101,6 @@ export default function ProfileView() {
     };
 
     useEffect(() => {
-        console.log('Change : ', userInfo);
         if (userInfo.firstName) {
             setFunction('firstName', userInfo.firstName, 'input');
             setFunction('lastName', userInfo.lastName, 'input');
@@ -69,17 +121,64 @@ export default function ProfileView() {
         setFunction('lastName', userInfo.lastName, 'input');
         const h1b_req = userInfo.h1b_required === true ? 'Yes' : 'No';
         setFunction('h1b', h1b_req, 'input');
+        setFunction('firstNameValid', true, 'valid');
+        setFunction('lastNameValid', true, 'valid');
+        setFunction('h1bValid', true, 'valid');
     };
 
     const editPersonal = () => {
-        console.log(inputValue);
+        if (nameRegrex.test(firstName)) {
+            setFunction('firstNameValid', true, 'valid');
+        } else {
+            setFunction('firstNameValid', false, 'valid');
+        }
+
+        if (nameRegrex.test(lastName)) {
+            setFunction('lastNameValid', true, 'valid');
+        } else {
+            setFunction('lastNameValid', false, 'valid');
+        }
+
+        if (h1b !== '') {
+            setFunction('h1bValid', true, 'valid');
+        } else {
+            setFunction('h1bValid', false, 'valid');
+        }
+
+        const resume_uploaded = true;
+
+        if (
+            nameRegrex.test(firstName) &&
+            nameRegrex.test(lastName) &&
+            h1b !== ''
+        ) {
+            const h1b_required = h1b === 'Yes' ? true : false;
+            updateUserProfile({
+                variables: {
+                    input: {
+                        userId,
+                        firstName,
+                        lastName,
+                        h1b_required,
+                        resume_uploaded
+                    }
+                }
+            })
+                .then((data) => {
+                    refetch();
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+            setEditPersonalInfo(false);
+        }
     };
 
     return (
         <div className="container">
             <div className="row">
                 <div className="main_content">
-                    <Card>
+                    <Card className="personal-card">
                         <Card.Title className="profile-title" as="h3">
                             Personal Information
                             {!editPersonalInfo && (
